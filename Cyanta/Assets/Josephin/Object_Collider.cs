7730f76b966ab032acf9f6bb2ea07ddc11f0a1d1
Josephin Kröger <67104864+yoshi-pdf@users.noreply.github.com>
2020-07-09 16:27:36 +0200

    Collider
    Der liebe Object_Collider macht jetzt natürlich noch nicht viel (e.g. nur Objektlöschung bei Collision mit Glow Orbs derzeit). Das muss man in Zukunft dann umschreiben.

---
+++ Cyanta/Assets/Josephin/Object_Collider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Collider : MonoBehaviour {
+
+    void OnTriggerEnter (Collider other) {
+        /*es muss im statement dann bezug auf die GUI/Munitionsanzeige genommen werden und ggf. die Animation gestartet werden*/
+        if (other.gameObject.name == "Glow Orb") {
+            Debug.Log (this.name + " löscht " + other.gameObject.name);
+            Destroy (other.gameObject);
+        }
+        Debug.Log (this.name + " kollidiert mit " + other.gameObject.name);
+    }
+}
