43b2f9ce27f334ecfaaa53f438e935bd1b46d81f
Josephin Kröger <67104864+yoshi-pdf@users.noreply.github.com>
2020-08-15 22:55:34 +0200

    Update Place_fish.cs

--- Cyanta - The Game/Assets/Josephin/Place_fish.cs
+++ Cyanta - The Game/Assets/Josephin/Place_fish.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Place_fish : MonoBehaviour {
+    int[] xFish = { 3, 6, 0, 12, 51, 81, 67, 78, 63, 52, 42, 24, 30, 6, 14, 6, 18, 42};
+    int[] zFish = { 3, 12, 23, 27, 0, 3, 9, 20, 20, 15, 15, 20, 33, 36, 42, 54, 75, 55};
+
     // Start is called before the first frame update
     public GameObject fish;
     void Start () {
@@ -11,12 +14,12 @@ public class Place_fish : MonoBehaviour {
 
     // Update is called once per frame
     void placeFish () {
-        int random = Random.Range (0, 15);
+        //int random = Random.Range (0, xFish.Length);
 
-        for (int i = 0; i < random; i++) {
+        for (int i = 0; i < xFish.Length; i++) {
             GameObject clone = Instantiate (fish);
             clone.name = "Fish";
-            clone.transform.position = new Vector3 (Random.Range (0, 30), 0.55f, Random.Range (0, 30));
+            clone.transform.position = new Vector3 (xFish[i], 0.55f, zFish[i]);
             var rb = clone.GetComponent<Rigidbody> ();
             rb.useGravity = false;
             //clone.AddComponent<Rigidbody> ();
