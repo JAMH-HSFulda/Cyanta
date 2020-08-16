b9212f3cb6e9636cf06931e83b6457f575bd455b
Josephin Kröger <67104864+yoshi-pdf@users.noreply.github.com>
2020-08-16 15:14:27 +0200

    Placement Orbs

--- Cyanta - The Game/Assets/Josephin/Place_fish.cs
+++ Cyanta - The Game/Assets/Josephin/Place_fish.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Place_fish : MonoBehaviour {
-    int[] xFish = { 3, 6, 0, 12, 51, 81, 67, 78, 63, 52, 42, 24, 30, 6, 14, 6, 18, 42};
-    int[] zFish = { 3, 12, 23, 27, 0, 3, 9, 20, 20, 15, 15, 20, 33, 36, 42, 54, 75, 55};
+    int[] xFish = { 3, 6, 0, 12, 51, 81, 67, 78, 63, 52, 42, 24, 30, 6, 14, 6, 18, 42 };
+    int[] zFish = { 3, 12, 23, 27, 0, 3, 9, 20, 20, 15, 15, 20, 33, 36, 42, 54, 75, 55 };
 
     // Start is called before the first frame update
     public GameObject fish;
@@ -22,8 +22,9 @@ public class Place_fish : MonoBehaviour {
             clone.transform.position = new Vector3 (xFish[i], 0.55f, zFish[i]);
             var rb = clone.GetComponent<Rigidbody> ();
             rb.useGravity = false;
-            //clone.AddComponent<Rigidbody> ();
-            //var rigidbody_sphere = clone.GetComponent<Rigidbody>();
+            clone.AddComponent<BoxCollider> ();
+            var collider = clone.GetComponent<BoxCollider> ();
+            collider.isTrigger = true;
         }
     }
 }
