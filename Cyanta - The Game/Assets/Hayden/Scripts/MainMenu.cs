fd341bdb94cecf256f5acd48afc750e897b88200
HaydenKG <67265528+HaydenKG@users.noreply.github.com>
2020-08-26 16:53:07 +0200

    Added crossfade scene transition from menu to playing
    Menu still not best looking. Waiting for meeting to counsel

--- Cyanta - The Game/Assets/Hayden/Scripts/MainMenu.cs
+++ Cyanta - The Game/Assets/Hayden/Scripts/MainMenu.cs
@@ -5,8 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator transition;
+
+    public float transTime = 1;
     public void PlayGame() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex) {
+        //Play animation
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transTime);
+        //wait
+
+        SceneManager.LoadScene(levelIndex);
+        //Load scene
     }
 
     public void QuitGame() {
