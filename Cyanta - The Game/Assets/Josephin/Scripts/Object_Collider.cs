834dfbadf6347d790783beab5d53a3de0faecd89
Marci-8 <marcia.arnold@informatik.hs-fulda.de>
2020-08-31 16:09:09 +0200

    AudioUpdate (Munition)

--- Cyanta - The Game/Assets/Josephin/Scripts/Object_Collider.cs
+++ Cyanta - The Game/Assets/Josephin/Scripts/Object_Collider.cs
@@ -9,6 +9,11 @@ public class Object_Collider : MonoBehaviour {
         /*Es muss im statement dann bezug auf die GUI/Munitionsanzeige genommen werden und ggf. die Animation gestartet werden*/
         if (other.gameObject.name == "Glow Orb") {
             Debug.Log (this.name + " löscht " + other.gameObject.name);
+
+            //Sound beim Munition Einsammeln //Marcia
+            FindObjectOfType<audiomanager>().Play("MunitionSammeln");
+            //
+            
             Destroy (other.gameObject);
             //Bei Kontakt mit nem Glow - Orb erhöht sich die Anzahl von X-en bzw. die Schussanzahl verringert sich
             if (Ammo.counter < 5) {
