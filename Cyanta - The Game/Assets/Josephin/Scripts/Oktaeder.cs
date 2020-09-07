3abd0f579999d14329eae7a609890bc2fd6935cb
Josephin Kröger <67104864+yoshi-pdf@users.noreply.github.com>
2020-09-07 18:37:57 +0200

    Spinning gem
    Added a spinning mesh gem above the goal line w/ a controller that controls the spinning. u know. just splendid.

---
+++ Cyanta - The Game/Assets/Josephin/Scripts/Oktaeder.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oktaeder : MonoBehaviour {
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+    // Start is called before the first frame update
+    void Start () {
+        buildmesh ();
+        updateMesh ();
+    }
+
+    void buildmesh () {
+        mesh = new Mesh ();
+        GetComponent<MeshFilter> ().mesh = mesh;
+        vertices = new Vector3[] {
+            new Vector3 (0, 0, 0), //unten links
+            new Vector3 (0.5f, 1, 0.5f), //oben
+            new Vector3 (1, 0, 0), //unten rechts
+            new Vector3 (0.5f, -1, 0.5f), //unten
+            new Vector3 (0, 0, 1), //unten links unter OG
+            new Vector3 (1, 0, 1) //unten rechts unter OG
+
+        };
+        triangles = new int[] {
+            0,
+            1,
+            2, //oben OG
+            0,
+            2,
+            3, //unten OG
+            4,
+            1,
+            0, //oben links OG
+            4,
+            0,
+            3, //unten links OG
+            5,
+            1,
+            4, //unten oben unter OG
+            5,
+            4,
+            3, //unten unter unten OG
+            2,
+            1,
+            5, //rechts oben
+            2,
+            5,
+            3
+        };
+    }
+
+    void updateMesh () {
+        mesh.Clear ();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals ();
+    }
+
+}
---
+++ Cyanta - The Game/Assets/Josephin/Scripts/Spin_Around.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin_Around : MonoBehaviour {
+    public GameObject target;
+    public int speed;
+
+    Collider m_Collider;
+    Vector3 m_Center;
+    // Start is called before the first frame update
+    void Start () {
+        m_Collider = GetComponent<Collider> ();
+        m_Center = m_Collider.bounds.center;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        transform.RotateAround (m_Center, Vector3.up, speed * Time.deltaTime);
+
+    }
+}
