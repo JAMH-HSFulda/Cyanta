456622498c5db2311086d805de932a091c0a27a2
Josephin Kröger <67104864+yoshi-pdf@users.noreply.github.com>
2020-09-09 16:56:38 +0200

    Death Counter
    Wenn man runterfällt, wir ein Death counter nach dem ersten fall angezeigt. nach 3x fallen werden die Credits geladen.

--- Cyanta - The Game/Assets/Josephin/Scripts/Ammo.cs
+++ Cyanta - The Game/Assets/Josephin/Scripts/Ammo.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Ammo : MonoBehaviour {
-    //Man hat am Anfang 2 Schuss
-    public static int counter = 2;
+    //Man hat am Anfang 5 Schuss
+    public static int counter = 5;
     public int numOfHearts;
 
     public Image[] hearts;
--- Cyanta - The Game/Assets/Josephin/Scripts/Object_Collider.cs
+++ Cyanta - The Game/Assets/Josephin/Scripts/Object_Collider.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Object_Collider : MonoBehaviour {
 
+    public int deaths = 0;
+    public Text deathText;
+    public Image skull;
     public ParticleSystem confetti;
+
+    void Start () {
+        //Death Counter und Anzahl werden am Anfang nicht angezeigt, est wenn man runtergefallen ist!
+        skull.enabled = false;
+        deathText.enabled = false;
+    }
 
     void OnTriggerEnter (Collider other) {
         /*Es muss im statement dann bezug auf die GUI/Munitionsanzeige genommen werden und ggf. die Animation gestartet werden*/
@@ -30,8 +40,21 @@ public class Object_Collider : MonoBehaviour {
             SceneManager.LoadScene (2);
         }
         if (other.gameObject.name == "Finishline") {
-           confetti.GetComponent<ParticleSystem> ().Play();
+            confetti.GetComponent<ParticleSystem> ().Play ();
+        }
+        if (other.gameObject.name == "Ziellandschaft") {
+            deaths += 1;
+
+            skull.enabled = true;
+            deathText.enabled = true;
+
+            deathText.text = deaths.ToString ();
+
+            if (deaths == 3) {
+                SceneManager.LoadScene (2);
+            }
+
+            //Vielleicht noch nen kleinen "Schrei" abspielen, wenn der Pinguin fällt?
         }
     }
-
 }
