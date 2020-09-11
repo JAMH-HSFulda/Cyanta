0a71ad0477ddc11661f7683b59d68377b048fcb7
HaydenKG <67265528+HaydenKG@users.noreply.github.com>
2020-09-11 15:44:29 +0200

    animations v2

---
+++ Cyanta - The Game/Assets/animSc.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animSc : MonoBehaviour
+{
+    Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animation>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.Play("idle");
+        if (Input.GetKeyDown("space")) {
+            anim.Play("jump");
+        }
+        
+    }
+}
