219b1373d5afb4aa8c3ff6b369225daa9b92422d
Josephin Kröger <67104864+yoshi-pdf@users.noreply.github.com>
2020-09-25 18:06:43 +0200

    Fly little fish FLYYY
    Die fische schweben jetzt hoch und runter, wenn man sich ihnen nähert "saugt" man sie an wie ein magnet.

---
+++ Cyanta - The Game/Assets/Josephin/Scripts/Fish_Controller.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish_Controller : MonoBehaviour {
+    private float timer = 0.0f;
+    private float visualTime = 0.0f;
+    private float waitTime = 3.6f;
+
+    public float distance;
+    public GameObject player;
+    public float speed_magnet = 5f;
+
+    public float speed_up = 0.2f;
+    public float height = 2.0f;
+
+    private int up;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start () {
+        rb = GetComponent<Rigidbody> ();
+        player = GameObject.Find ("Player");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        distance = Vector3.Distance (transform.position, player.transform.position);
+        timer += Time.deltaTime;
+
+        if (distance < 3) {
+            float step = speed_magnet * Time.deltaTime;
+            transform.position = Vector3.MoveTowards (transform.position, player.transform.position, step);
+        }
+
+        Vector3 startposition = transform.position;
+        float newY = Mathf.Sin (Time.time * speed_up);
+        transform.position = new Vector3 (startposition.x, newY, startposition.z) * height;
+
+        if (timer > waitTime) {
+            int random_spin = Random.Range (1, 15);
+            if (random_spin % 2 == 0) {
+                random_spin = random_spin * -1;
+            }
+            rb.transform.Rotate (0, random_spin, 0, Space.Self);
+
+            visualTime = timer;
+            // Remove the recorded 0.5 seconds.
+            timer = timer - waitTime;
+        }
+
+    }
+}
