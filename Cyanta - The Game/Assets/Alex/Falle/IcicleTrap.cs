43e8d176ab65e695cf123d3c31d8b0bd55b2a6ad
AlexSteckler <steckler-alex@t-online.de>
2020-10-01 02:28:09 +0200

    Traps placed

--- Cyanta - The Game/Assets/Alex/Falle/IcicleTrap.cs
+++ Cyanta - The Game/Assets/Alex/Falle/IcicleTrap.cs
@@ -9,11 +9,12 @@ public class IcicleTrap : MonoBehaviour
     public List<GameObject> ListIcicle;
     public List<Vector3> actualPos;
     public float startHeight = 4f, distanceToPlayer = 5f;
-    public float time, x, z, upTime;
-
+    public float x, z, upTime;
+    private float time;
+    public bool rotate = false;
     public Mesh mesh;
 
-    public Vector3 x0, x1, x2, x3, x4;
+    // public Vector3 x0, x1, x2, x3, x4;
  
     public bool[] boolArray;   
     // private GameObject[][] IcicleArr = new int[number][];
@@ -22,13 +23,9 @@ public class IcicleTrap : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-        // GameObject[][] IcicleArr = new int[numTraps][,];
-
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 3; j++)  {
                 clone = Instantiate(icicle, new Vector3(i + icicle.transform.position.x , icicle.transform.position.y + startHeight, j + icicle.transform.position.z), icicle.transform.rotation);
-                // MeshRenderer meshRender = clone.AddComponent<MeshRenderer>();
                 
                 clone.AddComponent<BoxCollider>();
                 clone.AddComponent<Rigidbody>();
@@ -37,8 +34,6 @@ public class IcicleTrap : MonoBehaviour
                 clone.GetComponent<BoxCollider>().center = new Vector3(0.5f, 3.8f, 0.5f);
                 clone.GetComponent<BoxCollider>().isTrigger = true;
                 clone.GetComponent<MeshRenderer>().material = new Material(Shader.Find("Diffuse"));
-                // Bounds bounds = clone.GetComponent<MeshFilter>().mesh.bounds;
-                // float yHeight = clone.GetComponent<MeshFilter>().sharedMesh.bounds.size.y;
                 clone.GetComponent<BoxCollider>().size = new Vector3(1, 2, 1);
                 clone.GetComponent<Rigidbody>().useGravity = false;
                 clone.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
@@ -55,16 +50,17 @@ public class IcicleTrap : MonoBehaviour
         }
 
         gameObject.transform.position = new Vector3(x, 1, z);
+
+        if (rotate) {
+            gameObject.transform.eulerAngles = new Vector3( gameObject.transform.eulerAngles.x, 90, gameObject.transform.eulerAngles.z );
+        }
+        
         icicle.transform.parent = gameObject.transform;
 
         for (int i = 0; i < ListIcicle.Count; i++) {
             actualPos.Add(ListIcicle[i].transform.position);
         }
-
-
     }
-
-    
 
     // Update is called once per frame
     void FixedUpdate()
@@ -97,19 +93,7 @@ public class IcicleTrap : MonoBehaviour
                     ListIcicle[i].GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
                     ListIcicle[i].GetComponent<Rigidbody>().useGravity = false;   
                 }
-                // if(ListIcicle[i].transform.position == actualPos[i]) {
-                //     Debug.Log("Funzt " + i);  
-                // }
             }  
-        //     if (time > upTime) {
-        //     time = 0;
-        // } 
         }
-        
- 
-        
-
-        
-
     }
 }
